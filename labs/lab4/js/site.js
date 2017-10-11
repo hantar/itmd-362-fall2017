6ffc9a05a772baa8f0b98c0af20cc4c676f8e6fb
Hidra <hantar@hawk.iit.edu>
2017-10-10 23:10:05 -0500

    add jQuery actions

--- labs/lab4/js/site.js
+++ labs/lab4/js/site.js
@@ -1,5 +1,13 @@
 // jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
+$(document).ready(function() {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  $('#submit').prop('disabled', true);
+  $('#age-verification').click(function() {
+    if($('#submit').prop('disabled')) {
+      $('#submit').prop('disabled', false).toggleClass('enabled');
+    } else {
+      $('#submit').prop('disabled', true).toggleClass('enabled');
+    }
+  })
 });
